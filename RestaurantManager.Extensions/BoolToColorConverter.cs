be5b9af48a9ee9d2e612e7c0351b9011e5948e2f
Imad Rahmouni <rahmounimad@gmail.com>
2016-03-15 21:45:45 +0100

    Homework 3 Converters and behaviours

---
+++ RestaurantManager.Extensions/BoolToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+
+namespace RestaurantManager.Extensions
+{
+    public class BoolToColorConverter: IValueConverter
+    {
+        public Color TrueColor { get; set; }
+        public Color FalseColor { get; set; }
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is bool) return (bool)value ? TrueColor : FalseColor;
+            return FalseColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return (value is Color && (Color) value == TrueColor);
+        }
+    }
+}
---
+++ RestaurantManager.Extensions/RightClickMessageDialogBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+using Microsoft.Xaml.Interactivity;
+
+namespace RestaurantManager.Extensions
+{
+    public class RightClickMessageDialogBehaviour :DependencyObject, IBehavior
+    {
+        public String Message { get; set; }
+        public String Title { get; set; }
+        public void Attach(DependencyObject associatedObject)
+        {
+            if (associatedObject is Page)
+            {
+                AssociatedObject = associatedObject;
+                (AssociatedObject as Page).RightTapped += OnRightTapped;
+            }
+      
+        }
+
+        private async void OnRightTapped(object sender, RightTappedRoutedEventArgs rightTappedRoutedEventArgs)
+        {
+            await new MessageDialog(Message,Title).ShowAsync();
+        }
+
+        public void Detach()
+        {
+            var page = AssociatedObject as Page;
+            if (page != null) page.RightTapped -= OnRightTapped;
+        }
+
+        public DependencyObject AssociatedObject { get; private set; }
+    }
+}
